7e502852d06b9ed26d098bfd5b41ff849d8d41f1
princewillzz <princewillz2013@gmail.com>
2020-05-09 13:37:20 +0530

    login authentication

--- web_chat/settings.py
+++ web_chat/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'users.apps.UsersConfig',
+    'chatting.apps.ChattingConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
